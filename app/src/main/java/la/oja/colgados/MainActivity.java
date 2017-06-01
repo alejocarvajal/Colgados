994890d1b2df6972a5789851993b7311509f220b
Alejandro Carvajal <alejo.carvajal03@gmail.com>
2017-06-01 10:17:42 -0500

    Actualizacion proyecto

--- app/src/main/java/la/oja/colgados/MainActivity.java
+++ app/src/main/java/la/oja/colgados/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,7 +15,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void iniciarPartidaUnJugador(View v){
+    void iniciarPartidaUnJugador(View v){
         Intent intento = new Intent(this,JuegoActivity.class);
         startActivity(intento);
     }
